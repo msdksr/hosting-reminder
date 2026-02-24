5d7e244000c40cee0d16edbca382b7c5714240f0
Masud Kaisar <masudkaisar@pinwheel.com>
2026-02-25 02:52:23 +0600

    chore: Disable React Strict Mode and suppress hydration warnings.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable}`}>
+      <body className={`${inter.variable}`} suppressHydrationWarning>
         <SessionProvider>
           <AppLayout>{children}</AppLayout>
         </SessionProvider>
